762130b60a6478bf4a4ceea033d890064afe3451
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-04-28 22:00:23 +0900

    게임 코드 수정 및 이미지 추가
    - src/260426/game.py: 게임 동작 관련 코드 개선 및 버그 수정
    - src/260426/fighter_small.png: 게임용 이미지 파일 추가

--- src/260426/game.py
+++ src/260426/game.py
@@ -4,19 +4,34 @@ pygame.init()
 
 screen = pygame.display.set_mode((400, 300))
 
-pic = pygame.image.load("fighter.png")
+pic = pygame.image.load("fighter_small.png")
 
 screen.fill( (123, 45, 240) )
 pygame.draw.rect(screen, (255, 0, 0), (110, 20, 120, 130) )
-screen.blit(pic, (10, 10, 30, 30))
-pygame.display.flip()
 
+x, y = 10, 10
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+                pygame.quit()
+                break
+            elif event.key == pygame.K_LEFT:
+                x -= 10
+            elif event.key == pygame.K_RIGHT:
+                x += 10
+
+    if not running:
+        break
+
+    screen.blit(pic, (x, y))
+    pygame.display.flip()
 
 
-print("Hello")
+print("Program has ended")
+
