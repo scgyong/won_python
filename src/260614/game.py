f90837452cc09746a2fc1f3bc74b49b1026f1163
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:30:25 +0900

    이미지를 객체 소유가 아닌, 클래스 소유가 되게 해 본다. 하지만 에러가 난다.
        image = pygame.image.load('meteor.png').convert_alpha()
    pygame.error: cannot convert without pygame.display initialized

--- src/260614/game.py
+++ src/260614/game.py
@@ -3,13 +3,13 @@ import random
 
 # Meteor 클래스 정의
 class Meteor:
+    image = pygame.image.load('meteor.png').convert_alpha()
     def __init__(self, x=None, y=None, dx=None, dy=None):
         if x is None: x = random.randint(100, 700)
         if y is None: y = random.randint(100, 500)
         if dx is None: dx = random.choice([-1, -0.5, 0.5, 1])
         if dy is None: dy = random.choice([-1, -0.5, 0.5, 1])
         self.x, self.y, self.dx, self.dy = x, y, dx, dy
-        self.image = pygame.image.load('meteor.png').convert_alpha()
     def move(self):
         self.x += self.dx
         if self.x >= 736 or self.x <= 0:
