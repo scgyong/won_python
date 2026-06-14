8092319a73e74ffd366df4da962e4110d0980a32
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:32:44 +0900

    Meteor.image = None 을 설정해 두고 객체 생성시 이미지가 없으면 로드한다.

--- src/260614/game.py
+++ src/260614/game.py
@@ -9,13 +9,15 @@ pygame.display.set_caption("운석 피하기")
 
 # Meteor 클래스 정의
 class Meteor:
-    image = pygame.image.load('meteor.png').convert_alpha()
+    image = None
     def __init__(self, x=None, y=None, dx=None, dy=None):
         if x is None: x = random.randint(100, 700)
         if y is None: y = random.randint(100, 500)
         if dx is None: dx = random.choice([-1, -0.5, 0.5, 1])
         if dy is None: dy = random.choice([-1, -0.5, 0.5, 1])
         self.x, self.y, self.dx, self.dy = x, y, dx, dy
+        if Meteor.image is None:
+            Meteor.image = pygame.image.load('meteor.png').convert_alpha()
     def move(self):
         self.x += self.dx
         if self.x >= 736 or self.x <= 0:
