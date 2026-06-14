d03f1d273ef97e34ca3e242023518cc28c563efa
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:43:09 +0900

    min/max 설정.

--- src/260614/meteor.py
+++ src/260614/meteor.py
@@ -1,23 +1,28 @@
 import pygame
 import random
+import cfg
 
 # Meteor 클래스 정의
 class Meteor:
     image = None
     def __init__(self, x=None, y=None, dx=None, dy=None):
-        if x is None: x = random.randint(100, 700)
-        if y is None: y = random.randint(100, 500)
+        if Meteor.image is None:
+            Meteor.image = pygame.image.load('meteor.png').convert_alpha()
+            Meteor.min_x = 0
+            Meteor.max_x = cfg.screen_width - Meteor.image.get_width()
+            Meteor.min_y = 0
+            Meteor.max_y = cfg.screen_height - Meteor.image.get_height()
+        if x is None: x = random.randint(Meteor.min_x, Meteor.max_x)
+        if y is None: y = random.randint(Meteor.min_y, Meteor.max_y)
         if dx is None: dx = random.choice([-1, -0.5, 0.5, 1])
         if dy is None: dy = random.choice([-1, -0.5, 0.5, 1])
         self.x, self.y, self.dx, self.dy = x, y, dx, dy
-        if Meteor.image is None:
-            Meteor.image = pygame.image.load('meteor.png').convert_alpha()
     def move(self):
         self.x += self.dx
-        if self.x >= 736 or self.x <= 0:
+        if self.x >= Meteor.max_x or self.x <= Meteor.min_x:
             self.dx = -self.dx
         self.y += self.dy
-        if self.y >= 536 or self.y <= 0:
+        if self.y >= Meteor.max_y or self.y <= Meteor.min_y:
             self.dy = -self.dy
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
