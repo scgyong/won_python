2034fc494aa5de0c9958c61791935a7c44aca45a
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:47:04 +0900

    meteor.x, meteor.y 가 이미지의 중심이 되게 한다.

--- src/260614/meteor.py
+++ src/260614/meteor.py
@@ -8,10 +8,12 @@ class Meteor:
     def __init__(self, x=None, y=None, dx=None, dy=None):
         if Meteor.image is None:
             Meteor.image = pygame.image.load('meteor.png').convert_alpha()
-            Meteor.min_x = 0
-            Meteor.max_x = cfg.screen_width - Meteor.image.get_width()
-            Meteor.min_y = 0
-            Meteor.max_y = cfg.screen_height - Meteor.image.get_height()
+            Meteor.half_width = Meteor.image.get_width() // 2
+            Meteor.half_height = Meteor.image.get_height() // 2
+            Meteor.min_x = Meteor.half_width
+            Meteor.max_x = cfg.screen_width - Meteor.half_width
+            Meteor.min_y = Meteor.half_height
+            Meteor.max_y = cfg.screen_height - Meteor.half_height
         if x is None: x = random.randint(Meteor.min_x, Meteor.max_x)
         if y is None: y = random.randint(Meteor.min_y, Meteor.max_y)
         if dx is None: dx = random.choice([-1, -0.5, 0.5, 1])
@@ -25,4 +27,4 @@ class Meteor:
         if self.y >= Meteor.max_y or self.y <= Meteor.min_y:
             self.dy = -self.dy
     def draw(self, screen):
-        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.image, (self.x - self.half_width, self.y - self.half_height))
