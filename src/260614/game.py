d1952ad82706f5f4d743aa7fb2c3d1fdb09503de
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:14:52 +0900

    Meteor 클래스 초기 작업.

--- src/260614/game.py
+++ src/260614/game.py
@@ -1,6 +1,24 @@
 import pygame
+import random
 
 # Meteor 클래스 정의
+class Meteor:
+    def __init__(self, x=None, y=None, dx=None, dy=None):
+        if x is None: x = random.randint(100, 700)
+        if y is None: y = random.randint(100, 500)
+        if dx is None: dx = random.choice([-1, -0.5, 0.5, 1])
+        if dy is None: dy = random.choice([-1, -0.5, 0.5, 1])
+        self.x, self.y, self.dx, self.dy = x, y, dx, dy
+    def move(self):
+        self.x += self.dx
+        if self.x >= 736 or self.x <= 0:
+            self.dx = -self.dx
+        self.y += self.dy
+        if self.y >= 536 or self.y <= 0:
+            self.dy = -self.dy
+    def draw(self, screen):
+        pass
+
 # Fighter 클래스 정의
 # pygame 초기화
 # 게임 루프
