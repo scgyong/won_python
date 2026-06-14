058323e258233610a6404cc6773930be101f8f2f
KYKim@MBP16 <scgyong@tukorea.ac.kr>
2026-06-14 12:21:20 +0900

    ESC Key 에도 프로그램 종료하게 하기.

--- src/260614/game.py
+++ src/260614/game.py
@@ -43,5 +43,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
 pygame.quit()
